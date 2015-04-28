a6a6dd3a8af246f9f3103bb8b86593446e05ca31
the3venthoriz0n <MrPC@Andrews-MacBook-Pro-3.local>
2015-04-28 01:55:36 -0400

    made a whole bunch of changes, total program changes

--- bot.py
+++ bot.py
@@ -1,21 +1,93 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#Andrew Kaplan Twitter Bot final project Social Software 4/28/15
 
+import tweepy, time, sys, random, json, collections
+import config as cfg #import config file with authentication information
 
-import tweepy, time, sys
-import config as cfg
-
-argfile = str(sys.argv[1])
-
+#----------------------AUTHENTICATION------------------------------------
 auth = tweepy.OAuthHandler(cfg.CONSUMER_KEY, cfg.CONSUMER_SECRET)
 auth.set_access_token(cfg.ACCESS_KEY, cfg.ACCESS_SECRET)
 api = tweepy.API(auth)
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
+if  True: # api.verify_credentials():
+	print "Your credentials are O.K."
 
-for line in f:
-    api.update_status(status = line)
-    #time.sleep(120)#Tweet every 2 minutes
-    time.sleep(900)#Tweet every 15 minutes
+	def _decode_list(data): #functions for decoding the unicode json object returned by the api.trends_available() method
+	    rv = []
+	    for item in data:
+	        if isinstance(item, unicode):
+	            item = item.encode('utf-8')
+	        elif isinstance(item, list):
+	            item = _decode_list(item)
+	        elif isinstance(item, dict):
+	            item = _decode_dict(item)
+	        rv.append(item)
+	    return rv
+
+	def _decode_dict(data):
+	    rv = {}
+	    for key, value in data.iteritems():
+	        if isinstance(key, unicode):
+	            key = key.encode('utf-8')
+	        if isinstance(value, unicode):
+	            value = value.encode('utf-8')
+	        elif isinstance(value, list):
+	            value = _decode_list(value)
+	        elif isinstance(value, dict):
+	            value = _decode_dict(value)
+	        rv[key] = value
+	    return rv
+
+	trend = _decode_list(api.trends_available()) # convert unicode reply to normal utf-8 list, assign to variable trend
+	for item in trend: #add name and yahoo world id for each item in trends list to dictionary of key value pairs
+		trenDic = {}
+		trenDic[(item['name'])] = item['woeid']
+		#print trenDic
+
+	topTen = _decode_list(api.trends_place((random.choice(trenDic.values())))) #choose random value(woeid) from dictionary
+	try:
+		print "TYPE TEST: ", type(topTen)
+		topTenList = []
+		topTenList.append(topTen.get('trends')) #append value for trends key in topTenList
+		print topTenList
+	except KeyError:
+		print "Key Error"
+
+	#follow every follower (A CRY FOR FRIENDSHIP!)
+	# for follower in tweepy.Cursor(api.followers).items():
+	#     follower.follow()
+
+	#api.trends_place(id[, exclude])
+	#if api.exists_friendship(notTooPopular, user_b) # true if user a follows user b
+	# api.followers_ids(id/screen_name/user_id) #returns id of followers 
+
+	#     #time.sleep(120)#Tweet every 2 minutes
+	#     time.sleep(900)#Tweet every 15 minutes (15*60)
+else:
+	print "Your credentials are incorrect! Check the config file"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
